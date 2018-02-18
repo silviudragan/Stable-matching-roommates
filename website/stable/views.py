07beba8ebb0551db6d8105e92c0adaeb966f4e4d
Dragan Silviu <silviu.dragan@gmail.com>
2018-02-18 19:12:51 +0200

    imbunatatiri register

--- website/stable/views.py
+++ website/stable/views.py
@@ -1,3 +1,5 @@
+import MySQLdb
+from django.db.backends import mysql
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import UserForm, LoginForm
@@ -38,17 +40,34 @@ class UserFormView(View):
 
     def get(self, request):
         form = self.form_class(None)
-
+        studenti = ""
         return render(request, self.template_name, {'form': form})
 
     def post(self, request):
         form = UserForm(request.POST)
-
+        re_password = request.POST.get('re_password', '')
         if form.is_valid():
             user = form.save(commit=False)
 
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
+            if password != re_password:
+                return render(request, self.template_name, {'form': form})
+            conn = MySQLdb.connect(host="localhost",
+                                 user="root",
+                                 passwd="Silviu01",
+                                 db="test")
+            c = conn.cursor()
+            c.execute("SELECT * FROM stable_student")
+            studenti = c.fetchall()
+            # verificam daca numarul matricol este valid
+            id_valid = False
+            for student in studenti:
+                if username in student:
+                    id_valid = True
+
+            if not id_valid:
+                return render(request, self.template_name, {'form': form, 'mesaj': "Numarul matricol nu este valid."})
 
             user.set_password(password)
             user.save()
