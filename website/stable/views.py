17ce6484928812cfc954b66f281f3feb46521378
Dragan Silviu <silviu.dragan@gmail.com>
2018-02-23 00:19:40 +0200

    date profil utilizator

--- website/stable/views.py
+++ website/stable/views.py
@@ -5,6 +5,11 @@ from django.shortcuts import render, redirect
 from .forms import UserForm, LoginForm
 from django.views.generic import View
 from django.contrib.auth import authenticate, login, logout
+username = ""
+conn = MySQLdb.connect(host="localhost",
+                                 user="root",
+                                 passwd="Silviu01",
+                                 db="test")
 
 
 def index(request):
@@ -23,6 +28,7 @@ class Login(View):
         return render(request, self.template_name, {'form': form})
 
     def post(self, request):
+        global username
         username = request.POST.get('nr_matricol', '')
         password = request.POST.get('parola', '')
         user = authenticate(username=username, password=password)
@@ -37,6 +43,7 @@ class Login(View):
 class UserFormView(View):
     form_class = UserForm
     template_name = 'stable/register.html'
+    global conn
 
     def get(self, request):
         form = self.form_class(None)
@@ -47,16 +54,14 @@ class UserFormView(View):
         form = UserForm(request.POST)
         re_password = request.POST.get('re_password', '')
         if form.is_valid():
+            global username
             user = form.save(commit=False)
 
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
             if password != re_password:
                 return render(request, self.template_name, {'form': form})
-            conn = MySQLdb.connect(host="localhost",
-                                 user="root",
-                                 passwd="Silviu01",
-                                 db="test")
+
             c = conn.cursor()
             c.execute("SELECT * FROM stable_student")
             studenti = c.fetchall()
@@ -90,7 +95,12 @@ class Profil(View):
     template_name = 'stable/profil.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        global username
+
+        c = conn.cursor()
+        c.execute("SELECT * FROM stable_student where numar_matricol= %s", [username])
+        student = c.fetchall()
+        return render(request, self.template_name, {'student': student})
 
     def post(self, request):
         pass
