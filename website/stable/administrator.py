6b732e7fd5ced340f98e363d92fa025d1f4db6f8
Dragan Silviu <silviu.dragan@gmail.com>
2018-04-26 23:21:05 +0300

    stable for 3

--- website/stable/administrator.py
+++ website/stable/administrator.py
@@ -1,3 +1,4 @@
+import copy
 from pprint import pprint
 from random import shuffle
 
@@ -11,6 +12,7 @@ conn = MySQLdb.connect(host="localhost",
                        passwd="Silviu01",
                        db="test")
 students = []
+copie_students = []
 camine = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C10', 'C11', 'C12', 'C13', 'Gaudeamus', 'Akademos',
           'Buna Vestire']
 
@@ -65,6 +67,8 @@ def incarcare_preferinte(camin):
     print('########################################')
     pprint(students)
     print('########################################')
+    global copie_students
+    copie_students = copy.deepcopy(students)
 
 
 def free(option):
@@ -103,6 +107,108 @@ def abandon_current_accept(option):
                     st_rm['propose'] = ""
 
 
+def exista_in_cheie(punctaje_perechi, nume):
+    for item in punctaje_perechi.keys():
+        if nume in item:
+            return True
+    return False
+
+
+def verificare_tag_empty():
+    for st in students:
+        if st['name'] == 'empty':
+            return True
+    return False
+
+
+def creare_perechi():
+    # aflam punctajul pentru preferinta fiecarei persoane; indexul persoanei repartizate din preferences
+    punctaje = dict()
+    for student in students:
+        if student['preferences'][0] == 'empty':
+            punctaje[student['name']] = 100
+        else:
+            for st in copie_students:
+                if st['name'] == student['name']:
+                    punctaje[student['name']] = st['preferences'].index(student['preferences'][0])
+
+    # calculam punctajul pentru fiecare pereche in parte
+    punctaje_perechi = dict()
+    for student in students:
+        if not exista_in_cheie(punctaje_perechi, student['name']):
+            # print(student)
+            key = student['name'] + "+" + student['preferences'][0]
+            punctaje_perechi[key] = punctaje[student['name']]
+            for st in copie_students:
+                if st['name'] == student['preferences'][0]:
+                    punctaje_perechi[key] += punctaje[st['name']]
+    if len(students) % 3 == 1 and verificare_tag_empty():
+        '''
+        trebuie doar eliminat empty din perechea corespunzatoare
+        '''
+        # print(punctaje_perechi)
+        for item in punctaje_perechi.keys():
+            if 'empty' in item:
+                new_key = item.replace('empty', '')
+                new_key = new_key.replace('+', '')
+                punctaje_perechi[new_key] = punctaje_perechi.pop(item)
+        # print(punctaje_perechi)
+    else:
+        '''
+        trebuie adaugate empty multiplu de 3
+        '''
+    # print(punctaje_perechi)
+    preferinte_pentru_stable_3(punctaje_perechi)
+
+
+def preferinte_pentru_stable_3(punctaje_perechi):
+    perechi = []
+    single = []
+    for item in punctaje_perechi.keys():
+        if '+' in item:
+            perechi.append(item)
+        else:
+            single.append(item)
+
+    preferinte = dict()
+    for item in perechi:
+        st1 = item.split('+')[0]
+        st2 = item.split('+')[1]
+        optiuni = []
+        for student in copie_students:
+            if student['name'] == st1:
+                preferences_st1 = copy.copy(student['preferences'])
+            elif student['name'] == st2:
+                preferences_st2 = copy.copy(student['preferences'])
+        i = 0
+        j = 0
+        while i < len(preferences_st1) and j < len(preferences_st2):
+            if preferences_st1[i] in single and preferences_st1[i] not in optiuni:
+                optiuni.append(preferences_st1[i])
+            if preferences_st2[j] in single and preferences_st2[j] not in optiuni:
+                optiuni.append(preferences_st2[j])
+            i += 1
+            j += 1
+        preferinte[item] = optiuni
+
+    for item in single:
+        for student in copie_students:
+            if student['name'] == item:
+                preferences = copy.copy(student['preferences'])
+
+        optiuni = []
+        for p in perechi:
+            st1 = p.split('+')[0]
+            st2 = p.split('+')[1]
+
+            if preferences.index(st1) < preferences.index(st2):
+                optiuni.append(st1)
+            else:
+                optiuni.append(st2)
+        preferinte[item] = optiuni
+    print(preferinte)
+
+
 def stable(camin):
     incarcare_preferinte(camin)
     while True:
@@ -137,7 +243,7 @@ def stable(camin):
             to_delete = st['preferences'][st['preferences'].index(st['accept'])+1:]
         except Exception as error:
             print(error)
-        print(to_delete)
+        # print(to_delete)
         for it in to_delete:
             for i in students:
                 if it == i['name']:
@@ -195,14 +301,15 @@ class Administrator(View):
         return render(request, self.template_name)
 
     def post(self, request):
-        print("rec:", request)
-        print("self:", self)
         try:
             stable('C12')
         except:
             mesaj = "Ups, se pare ca ceva nu a mers bine, te rugam sa incerci din nou!"
             return render(request, self.template_name, {'mesaj_warning': mesaj})
-        pprint(students)
+        # pprint(students)
+        for student in students:
+            print(student['name'] + " -> " + student['preferences'][0] + " " + str(len(student['preferences'])))
+        creare_perechi()
         mesaj = "Repartizarea a fost facuta cu succes."
         return render(request, self.template_name, {'mesaj_succes': mesaj})
 
